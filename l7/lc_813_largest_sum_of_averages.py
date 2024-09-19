433329948f0f2aa05443cffb54e78404ad197ec1
will_bit <williamc.c999@gmail.com>
2024-09-20 01:54:20 +0800

    update

--- l7/lc_813_largest_sum_of_averages.py
+++ l7/lc_813_largest_sum_of_averages.py
@@ -3,21 +3,21 @@ from typing import List
 
 class Solution:
     def largestSumOfAverages(self, nums: List[int], k: int) -> float:
-        num_len = len(nums)
-        dp = [0] * (num_len + 1)
+        arr_len = len(nums)
+        prefix_sum = [0] * (arr_len + 1)
 
-        for i in range(num_len):
-            dp[i + 1] = dp[i] + nums[i]
+        for i in range(arr_len):
+            prefix_sum[i + 1] = prefix_sum[i] + nums[i]
 
-        dp = [0.0] * num_len
-        for i in range(num_len):
-            dp[i] = (dp[num_len] - dp[i]) / (num_len - i)
+        dp = [0.0] * arr_len
+        for i in range(arr_len):
+            dp[i] = (prefix_sum[arr_len] - prefix_sum[i]) / (arr_len - i)
 
-        for k in range(k - 1):
+        for _ in range(k - 1):
             new_dp = dp[:]
-            for i in range(num_len):
-                for j in range(i + 1, num_len):
-                    new_dp[i] = max(new_dp[i], (dp[j] - dp[i]) / (j - i) + dp[j])
+            for i in range(arr_len):
+                for j in range(i + 1, arr_len):
+                    new_dp[i] = max(new_dp[i], (prefix_sum[j] - prefix_sum[i]) / (j - i) + dp[j])
             dp = new_dp
 
         return dp[0]
