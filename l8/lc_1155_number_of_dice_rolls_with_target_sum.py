416aceff3026358abf8206fce45585a5dac823d2
will_bit <williamc.c999@gmail.com>
2024-09-24 09:10:44 +0800

    update

--- l8/lc_1155_number_of_dice_rolls_with_target_sum.py
+++ l8/lc_1155_number_of_dice_rolls_with_target_sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         MOD = 1000000007
-        memo = [[0] * (target + 1) for _ in range(n + 1)]
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
 
         # Initialize the base case
-        memo[n][target] = 1
+        dp[n][target] = 1
 
         for dice_index in range(n - 1, -1, -1):
             for curr_sum in range(target + 1):
@@ -12,8 +12,8 @@ class Solution:
 
                 # Iterate over the possible face values for the current die
                 for i in range(1, min(k, target - curr_sum) + 1):
-                    ways = (ways + memo[dice_index + 1][curr_sum + i]) % MOD
+                    ways = (ways + dp[dice_index + 1][curr_sum + i]) % MOD
 
-                memo[dice_index][curr_sum] = ways
+                dp[dice_index][curr_sum] = ways
 
-        return memo[0][0]
+        return dp[0][0]
